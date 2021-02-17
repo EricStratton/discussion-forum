198f63f3ec8511e0257c84b0c655be291b18dd4c
Eric Stratton <strattonericj@gmail.com>
2021-02-17 10:06:43 -0800

    commit actions directory with functional and tested action and constants
    co-authored-by: Patrick Osten <posten.coding@gmail.com>

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+import * as c from '../actions/ActionTypes';
+
+export const deletePost = id => ({
+  type: c.DELETE_POST,
+  id
+})
+
+export const addPost = (post) => {
+  const { content, votes, timeStamp, id } = post;
+  return {
+    type: c.ADD_POST,
+    content,
+    votes,
+    timeStamp,
+    id
+  }
+}
